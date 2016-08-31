84010a54cf7813cc04efe60231dbe8e0f38b9e00
onesang.you <sc545@mutecsoft.com>
2016-08-31 19:01:32 +0900

    second commit

--- app/src/main/java/com/example/k/seobangnim/MainActivity.java
+++ app/src/main/java/com/example/k/seobangnim/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.k.seobangnim;
 
+import android.app.TabActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+        tabHost.setup();
+
+        String[] str = {"[ Tab1 ]", "[ Tab2 ]", "[ Tab3 ]"};
+        Intent[] tabIntent = {new Intent(this, Tab1Activity.class), new Intent(this, Tab1Activity.class), new Intent(this, Tab1Activity.class)};
+        for (int i=0; i<3; i++) {
+            TabHost.TabSpec spec = tabHost.newTabSpec(str[i]).setContent(tabIntent[i]).setIndicator(str[i]);
+            tabHost.addTab(spec);
+            tabHost.getTabWidget().getChildAt(i).getLayoutParams().height = 180;
+        }
     }
 }
